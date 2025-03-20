1494af4fa5576624a02615b0b9b4b023f079a319
minhthang <minhthangqhqn@gmail.com>
2025-03-20 14:16:18 +0700

    fix layout

--- src/main/java/com/fsoft/fintern/services/CVInfoService.java
+++ src/main/java/com/fsoft/fintern/services/CVInfoService.java
@@ -2,21 +2,21 @@ package com.fsoft.fintern.services;
 
 import com.fsoft.fintern.models.CVInfo;
 import com.fsoft.fintern.models.EmbedableID.CV_InfoId;
-import com.fsoft.fintern.repositories.CVInfoRepo;
+import com.fsoft.fintern.repositories.CVInfoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CVInfoService {
-    private final CVInfoRepo cvInfoRepo;
+    private final CVInfoRepository cvInfoRepository;
 
-    public CVInfoService(CVInfoRepo cvInfoRepo) {
-        this.cvInfoRepo = cvInfoRepo;
+    public CVInfoService(CVInfoRepository cvInfoRepository) {
+        this.cvInfoRepository = cvInfoRepository;
     }
 
     @Transactional
     public CVInfo create(CV_InfoId id, Double gpa, String skill, String education) {
-        if (cvInfoRepo.existsById(id)) {
+        if (cvInfoRepository.existsById(id)) {
             throw new RuntimeException("CV đã tồn tại!");
         }
         CVInfo cvInfo = new CVInfo();
@@ -24,6 +24,6 @@ public class CVInfoService {
         cvInfo.setGpa(gpa);
         cvInfo.setSkill(skill);
         cvInfo.setEducation(education);
-        return cvInfoRepo.save(cvInfo);
+        return cvInfoRepository.save(cvInfo);
     }
 }
