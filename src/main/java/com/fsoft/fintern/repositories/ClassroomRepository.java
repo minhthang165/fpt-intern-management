f1d13b943b36107b64e5fe215474bab7a0d92924
AlexNguyenHere <155418110+AlexNguyenHere@users.noreply.github.com>
2025-03-26 13:33:05 +0700

    BE_Completed

--- src/main/java/com/fsoft/fintern/repositories/ClassroomRepository.java
+++ src/main/java/com/fsoft/fintern/repositories/ClassroomRepository.java
@@ -1,13 +1,21 @@
 package com.fsoft.fintern.repositories;
 
 import com.fsoft.fintern.models.Classroom;
+import com.fsoft.fintern.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ClassroomRepository extends JpaRepository<Classroom, Integer> {
     Optional<Classroom> findClassroomByClassName(String className);
-    Optional<Classroom> findClassroomById(Integer classId);
+    @Query(value = "SELECT COUNT(u.id) FROM [user] u WHERE u.class_id = :classId AND u.is_active = 1", nativeQuery = true)
+    Integer countUsersByClassIdAndIsActiveTrue(@Param("classId") Integer classId);
+    @Query(value = "SELECT * FROM [user] WHERE class_id = :classId AND is_active = 1", nativeQuery = true)
+    List<User> findByClassIdAndIsActiveTrue(@Param("classId") Integer classId);
+
 }
