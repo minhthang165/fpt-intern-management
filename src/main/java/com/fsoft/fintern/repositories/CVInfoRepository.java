575619305056be5ea57810775234dfd9b97ac10a
jronewo <trongboigaren2107@gmail.com>
2025-03-26 15:51:52 +0700

    thêm ô loading

--- src/main/java/com/fsoft/fintern/repositories/CVInfoRepository.java
+++ src/main/java/com/fsoft/fintern/repositories/CVInfoRepository.java
@@ -2,6 +2,7 @@ package com.fsoft.fintern.repositories;
 
 import com.fsoft.fintern.models.CVInfo;
 import com.fsoft.fintern.models.EmbedableID.CVInfoId;
+import com.fsoft.fintern.models.File;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -11,11 +12,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public interface CVInfoRepository extends JpaRepository<CVInfo, CVInfoId> {
     
     @Query("SELECT c FROM CVInfo c WHERE c.id.recruitmentId = :recruitmentId AND c.isActive = true")
     List<CVInfo> findAllByRecruitmentId(@Param("recruitmentId") Integer recruitmentId);
-    
+
 }
