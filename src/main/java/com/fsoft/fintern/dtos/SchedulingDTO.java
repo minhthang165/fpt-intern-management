00f30f258c15e563553665a5e6b17e126b56dbf2
AnhTuanFPT1303 <tuannha.ber133@gmail.com>
2025-04-03 13:09:06 +0700

    merge

--- src/main/java/com/fsoft/fintern/dtos/SchedulingDTO.java
+++ src/main/java/com/fsoft/fintern/dtos/SchedulingDTO.java
@@ -7,12 +7,62 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class SchedulingDTO {
+
+    public SchedulingDTO(String classId, String className, ClassType classType, LanguageType languageType, Integer roomId) {
+        this.classId = classId;
+        this.className = className;
+        this.classType = classType;
+        this.languageType = languageType;
+        this.roomId = roomId;
+    }
+
+    public SchedulingDTO() {
+    }
+
     private String classId;
     private String className;
     private ClassType classType;
     private LanguageType languageType;
     private Integer roomId;
-} 
+
+    public String getClassId() {
+        return classId;
+    }
+
+    public void setClassId(String classId) {
+        this.classId = classId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public ClassType getClassType() {
+        return classType;
+    }
+
+    public void setClassType(ClassType classType) {
+        this.classType = classType;
+    }
+
+    public LanguageType getLanguageType() {
+        return languageType;
+    }
+
+    public void setLanguageType(LanguageType languageType) {
+        this.languageType = languageType;
+    }
+
+    public Integer getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Integer roomId) {
+        this.roomId = roomId;
+    }
+}
