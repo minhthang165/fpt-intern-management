0620fcb131969b61c5f9e244c83090b3acf4c261
AnhTuanFPT1303 <tuannha.ber133@gmail.com>
2025-04-03 13:48:07 +0700

    rp

--- src/main/java/com/fsoft/fintern/dtos/ScheduleResultDTO.java
+++ src/main/java/com/fsoft/fintern/dtos/ScheduleResultDTO.java
@@ -7,12 +7,98 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class ScheduleResultDTO {
     private Integer id;
     private String className;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public ScheduleResultDTO(Integer id, String className, String subjectName, String roomName, String dayOfWeek, LocalTime startTime, LocalTime endTime, LocalDate endDate, LocalDate startDate) {
+        this.id = id;
+        this.className = className;
+        this.subjectName = subjectName;
+        this.roomName = roomName;
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.endDate = endDate;
+        this.startDate = startDate;
+    }
+
+    public ScheduleResultDTO() {
+    }
+
     private String subjectName;
     private String roomName;
     private String dayOfWeek;
--- src/main/java/com/fsoft/fintern/dtos/SchedulingDTO.java
+++ src/main/java/com/fsoft/fintern/dtos/SchedulingDTO.java
@@ -6,11 +6,58 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class SchedulingDTO {
     private String classId;
+
+    public SchedulingDTO(String classId, String className,  ClassType classType, LanguageType languageType,Integer roomId) {
+        this.classId = classId;
+        this.className = className;
+        this.languageType = languageType;
+        this.classType = classType;
+        this.roomId = roomId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getClassId() {
+        return classId;
+    }
+
+    public void setClassId(String classId) {
+        this.classId = classId;
+    }
+
+    public ClassType getClassType() {
+        return classType;
+    }
+
+    public void setClassType(ClassType classType) {
+        this.classType = classType;
+    }
+
+    public LanguageType getLanguageType() {
+        return languageType;
+    }
+
+    public void setLanguageType(LanguageType languageType) {
+        this.languageType = languageType;
+    }
+
+    public Integer getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Integer roomId) {
+        this.roomId = roomId;
+    }
+
     private String className;
     private ClassType classType;
     private LanguageType languageType;
